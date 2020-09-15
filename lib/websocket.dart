d23a9c11c31a942305e1e5bcb124365a70cdef72
smartwalle <smartwalle@gmail.com>
2020-09-15 14:08:07 +0800

    init

--- lib/websocket.dart
+++ lib/websocket.dart
@@ -1,1 +1,2 @@
-export 'package:sm_websocket/src/web.dart' if (dart.library.io) 'package:sm_websocket/io.dart';
+export 'package:sm_websocket/src/stub.dart';
+export 'package:sm_websocket/src/web.dart' if (dart.library.io) 'package:sm_websocket/src/io.dart';
